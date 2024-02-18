fce3a0f595ed38aaf46593aae805855fbefaa0c6
PHJoon <phj9503@gmail.com>
2024-02-19 07:45:41 +0900

    add 11053.js

---
+++ BOJ/11053.js
@@ -0,0 +1,34 @@
+const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('./예제.txt'),
+    // input: process.stdin,
+    output: process.stdout
+});
+
+const input = [];
+
+rl.on('line', function(line) {
+    input.push(line);
+}).on('close', function() {
+    const list = input.map((l) => l.split(' ').map((el) => parseInt(el)));
+    solution(list);
+    process.exit();
+});
+
+function solution (input) {
+    const [n] = input[0];
+    const sequence = input[1];
+    const dp = Array(n).fill(1);
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (sequence[i] > sequence[j]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    console.log(Math.max(...dp));
+}

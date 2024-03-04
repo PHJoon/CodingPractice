328fb6e07cb6250f1c5d0f6fa6365805bc99747b
PHJoon <phj9503@gmail.com>
2024-03-04 10:35:26 +0900

    add 1932.js

---
+++ BOJ/1932.js
@@ -0,0 +1,44 @@
+const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('./예제.txt'),
+    // input: process.stdin,
+    output: process.stdout
+});
+
+const input = [];
+
+rl.on('line', function(line) {
+    input.push(line);
+}).on('close', function() {
+    const list = input.map((l) => l.split(' ').map((el) => parseInt(el)));
+    solution(list);
+    process.exit();
+});
+
+function solution(input) {
+    const [n] = input[0];
+    const tri = Array(n);
+    for (let i = 0; i < n; i++) {
+        tri[i] = input[i + 1];
+    }
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j <= i; j++) {
+            if (i === 1) {
+                tri[i][j] += tri[0][0];
+                continue;
+            }
+            if (j === 0) {
+                tri[i][j] += tri[i - 1][j];
+            } else if (j === i) {
+                tri[i][j] += tri[i - 1][j - 1];
+            } else {
+                tri[i][j] += Math.max(tri[i - 1][j - 1], tri[i - 1][j]);
+            }
+        }
+    }
+
+    console.log(Math.max(...tri[n - 1]));
+}
